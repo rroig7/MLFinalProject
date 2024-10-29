5378bbaddd2854b7fa31cc4a68690b70cb388f88
rroig <rroig7@gmail.com>
2024-10-29 15:25:49 -0400

    we going connect 4 mode

--- README.md
+++
@@ -1,1 +0,0 @@
-We are going to recreate Geometry Dash from scratch and have an AI learn to play it.
--- action_control.py
+++
@@ -1,6 +0,0 @@
-import pyautogui
-
-# Map actions to actual keypresses
-def perform_action(action):
-    if action == 1:  # 1 means jump
-        pyautogui.press('space')
